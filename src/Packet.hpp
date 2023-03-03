d3251668f62c789db79e486775b22a8e012c75f5
AWann2 <awann@4tel.com.au>
2023-03-03 12:13:58 +1100

    Add conversion from javascript value to packet

--- src/Packet.hpp
+++ src/Packet.hpp
@@ -3,6 +3,9 @@
 
 #include <cstdint>
 
+#include "Hash.hpp"
+#include "Timestamp.hpp"
+
 namespace nbs {
     struct Packet {
         /// The packet timestamp in the NBS file
@@ -19,6 +22,53 @@ namespace nbs {
 
         /// The packet length in bytes, including the header
         uint32_t length;
+
+        /// Convert the given JS value to a timestamp in nanoseconds.
+        /// The JS value must contain the `timestamp`, `type`, `subtype` and `payload` properties.
+        static inline Packet FromJsValue(const Napi::Value jsPacket, const Napi::Env& env) {
+
+            if (!jsPacket.IsObject()) {
+                throw std::runtime_error("expected packet object");
+            }
+
+            auto jsObject = jsPacket.As<Napi::Object>();
+
+            // Check keys are valid
+            if (!jsObject.Has("timestamp")) {
+                throw std::runtime_error("expected object with `timestamp` key");
+            }
+            if (!jsObject.Has("type")) {
+                throw std::runtime_error("expected object with `type` key");
+            }
+            if (!jsObject.Has("subtype")) {
+                throw std::runtime_error("expected object with `subtype` key");
+            }
+            if (!jsObject.Has("payload")) {
+                throw std::runtime_error("expected object with `payload` key");
+            }
+
+            // Check types are valid
+            uint64_t timestamp = Timestamp::FromJsValue(jsObject.Get("timestamp"), env);
+            uint64_t type      = Hash::FromJsValue(jsObject.Get("type"), env);
+            if (!jsObject.Get("subtype").IsNumber()) {
+                throw std::runtime_error("expected `subtype` to be number");
+            }
+            if (!jsObject.Get("payload").IsBuffer()) {
+                throw std::runtime_error("expected `payload` to be buffer object");
+            }
+
+            auto payloadBuffer = jsObject.Get("payload").As<Napi::Buffer<uint8_t>>();
+            auto subtype       = jsObject.Get("subtype").As<Napi::Number>().Uint32Value();
+
+            Packet packet;
+            packet.timestamp = timestamp;
+            packet.type      = type;
+            packet.subtype   = subtype;
+            packet.length    = payloadBuffer.ByteLength();
+            packet.payload   = payloadBuffer.Data();
+
+            return packet;
+        }
     };
 }  // namespace nbs
 
